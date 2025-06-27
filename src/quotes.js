ea4c9575b45c305dbb35b4c08a6774d63f6e6b91
Max Teleshko <makakattttttt@gmail.com>
2025-06-27 23:02:09 +0300

    Splitting into modules first action

---
+++ src/quotes.js
@@ -0,0 +1,44 @@
+const quotes =[
+    {
+        quote:'The only way to do great work is to love what you do.',
+        author: 'Steve Jobs'
+    },
+    {
+        quote: 'Innovation distinguishes between a leader and a follower.',
+        author: 'Steve Jobs'
+    },
+    {
+        quote: "Your time is limited, so don't waste it living someone else's life.",
+        author: 'Steve Jobs'
+    },
+    {
+        quote: 'The only thing we have to fear is fear itself.',
+        author: 'Franklin D.Roosevelt'
+    },
+    {
+        quote: "Life is what happens when you're busy making other plans.",
+        author: 'John Lennon'
+    },    
+    {
+        quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+        author: 'Winston Churchill'
+    },
+    {
+        quote: 'The greatest glory in living lies not in never failing, but in rising every time we fall.',
+        author: 'Nelson Mandela'
+    },
+    {
+        quote: 'The future belongs to those who believe in the beauty of their dreams.',
+        author: 'Eleanor Roosevelt'
+    },
+    {
+        quote: 'Strive not to be a succes, but rather to be of value.',
+        author: 'Albert Einstein'
+    },
+    {
+        quote: "You miss 100% shots you don't take.",
+        author: 'Mayne Gretzky'
+    },            
+];
+
+export default quotes;
