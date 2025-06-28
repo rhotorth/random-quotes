0001c6d6524220860f8ea50793bfad61aee6d2e0
Max Teleshko <makakattttttt@gmail.com>
2025-06-28 22:26:00 +0300

    Favorite quotes system implication first action

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 /*      TODO:
 * -----------------------------
 *
-*   Add fauvorite quotes system
+*   Add favorite quotes system
 *   Spread this js file into modules
 *
 *-----------------------------
 */
 
 import quotes from "./src/quotes.js";
+
+
 
 const quoteElement = document.getElementById('quote');
 const generateButton = document.getElementById('generate-button');
@@ -16,28 +18,71 @@ const body = document.body;
 const container = document.getElementById('container-div');
 const heading1 = document.getElementById('h1');
 const authorElement = document.getElementById('author');
-
+const favoriteButton = document.getElementById('favorite-button')
 
 let previousRandomIndex;
+let currentIndex;
+
+
+function isFavoriteCheck() {
+    if (quotes[currentIndex].isFavorite) {
+            favoriteButton.textContent = 'Unfavorite quote'
+        } else {
+            favoriteButton.textContent = 'Favorite quote'
+        }
+}
 
 
 function generateRandomQuote() {
+    const currentBackgroundColor = getComputedStyle(body).backgroundColor;
     let randomIndex;
-    
+
+    if (currentBackgroundColor === 'rgb(240, 240, 240)') {
+        quoteElement.style.color = 'black';
+    } else {
+        quoteElement.style.color = '#f0f0f0';
+    }
+
     do {
         randomIndex = Math.floor(Math.random() * quotes.length)
     } while (randomIndex === previousRandomIndex)
         
     previousRandomIndex = randomIndex;
+    currentIndex = randomIndex;
+
+    isFavoriteCheck();
+
     const randomQuote = quotes[randomIndex];
-    const quote = `<em>"'${randomQuote.quote}"</em>`;
+    const quote = `<em>"${randomQuote.quote}"</em>`;
     quoteElement.innerHTML = quote;
     authorElement.innerHTML = randomQuote.author;
+
 }
 
 
-function switchMode() {
+function toggleFavorite () {
+    const currentBackgroundColor = getComputedStyle(body).backgroundColor;
     
+    try {
+        quotes[currentIndex].isFavorite = !quotes[currentIndex].isFavorite;
+        isFavoriteCheck();
+    } catch {
+        quoteElement.style.color = 'red';
+        quoteElement.textContent = 'Quote has not been generated yet.';
+        
+        setTimeout(() => {
+            if (currentBackgroundColor === 'rgb(240, 240, 240)') {
+                quoteElement.style.color = 'black';
+            } else {
+                quoteElement.style.color = '#f0f0f0';
+            }
+        }, 300);
+    }
+}
+
+
+
+function switchMode() {
     const currentBackgroundColor = getComputedStyle(body).backgroundColor;
 
     if (currentBackgroundColor === 'rgb(240, 240, 240)') {
@@ -50,6 +95,8 @@ function switchMode() {
         dayNightModeButton.style.color = '#f0f0f0';
         dayNightModeButton.textContent = 'Switch to light theme'
         authorElement.style.color = '#f0f0f0';
+        favoriteButton.style.backgroundColor = '#27272a';
+        favoriteButton.style.color = '#f0f0f0';
     
     } else {
         
@@ -61,10 +108,12 @@ function switchMode() {
         dayNightModeButton.style.backgroundColor = 'aliceblue';
         dayNightModeButton.textContent = 'Switch to dark theme';
         authorElement.style.color = 'black';
-
+        favoriteButton.style.backgroundColor = 'aliceblue';
+        favoriteButton.style.color = 'black';
     }
 }
 
 
 generateButton.addEventListener('click', generateRandomQuote);
 dayNightModeButton.addEventListener('click', switchMode);
+favoriteButton.addEventListener('click', toggleFavorite)
--- src/quotes.js
+++ src/quotes.js
@@ -1,43 +1,53 @@
 const quotes =[
     {
         quote:'The only way to do great work is to love what you do.',
-        author: 'Steve Jobs'
+        author: 'Steve Jobs',
+        isFavorite: false
     },
     {
         quote: 'Innovation distinguishes between a leader and a follower.',
-        author: 'Steve Jobs'
+        author: 'Steve Jobs',
+        isFavorite: false
     },
     {
         quote: "Your time is limited, so don't waste it living someone else's life.",
-        author: 'Steve Jobs'
+        author: 'Steve Jobs',
+        isFavorite: false
     },
     {
         quote: 'The only thing we have to fear is fear itself.',
-        author: 'Franklin D.Roosevelt'
+        author: 'Franklin D.Roosevelt',
+        isFavorite: false
     },
     {
         quote: "Life is what happens when you're busy making other plans.",
-        author: 'John Lennon'
+        author: 'John Lennon',
+        isFavorite: false
     },    
     {
         quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
-        author: 'Winston Churchill'
+        author: 'Winston Churchill',
+        isFavorite: false
     },
     {
         quote: 'The greatest glory in living lies not in never failing, but in rising every time we fall.',
-        author: 'Nelson Mandela'
+        author: 'Nelson Mandela',
+        isFavorite: false
     },
     {
         quote: 'The future belongs to those who believe in the beauty of their dreams.',
-        author: 'Eleanor Roosevelt'
+        author: 'Eleanor Roosevelt',
+        isFavorite: false
     },
     {
         quote: 'Strive not to be a succes, but rather to be of value.',
-        author: 'Albert Einstein'
+        author: 'Albert Einstein',
+        isFavorite: false
     },
     {
         quote: "You miss 100% shots you don't take.",
-        author: 'Mayne Gretzky'
+        author: 'Mayne Gretzky',
+        isFavorite: false
     },            
 ];
 
