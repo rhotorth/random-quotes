3132f693023b3bdad9d00c79e81baf75d4a07b37
Max Teleshko <makakattttttt@gmail.com>
2025-06-29 22:15:25 +0300

    Favorite quotes system implication second action

--- index.js
+++ index.js
@@ -13,22 +13,24 @@ import quotes from "./src/quotes.js";
 
 const quoteElement = document.getElementById('quote');
 const generateButton = document.getElementById('generate-button');
-const dayNightModeButton = document.getElementById('day-night-mode-button')
+const dayNightModeButton = document.getElementById('day-night-mode-button');
 const body = document.body;
 const container = document.getElementById('container-div');
 const heading1 = document.getElementById('h1');
 const authorElement = document.getElementById('author');
-const favoriteButton = document.getElementById('favorite-button')
+const favoriteButton = document.getElementById('favorite-button');
+const favoriteContainer = document.getElementById('favorite-container');
 
 let previousRandomIndex;
 let currentIndex;
+let favoriteCard;
 
 
 function isFavoriteCheck() {
     if (quotes[currentIndex].isFavorite) {
-            favoriteButton.textContent = 'Unfavorite quote'
+            favoriteButton.textContent = 'Unfavorite quote';
         } else {
-            favoriteButton.textContent = 'Favorite quote'
+            favoriteButton.textContent = 'Favorite quote';
         }
 }
 
@@ -66,10 +68,41 @@ function toggleFavorite () {
     try {
         quotes[currentIndex].isFavorite = !quotes[currentIndex].isFavorite;
         isFavoriteCheck();
-    } catch {
+
+        if (quotes[currentIndex].isFavorite) {  
+            const favoriteCard = document.createElement('div');
+            
+            favoriteCard.classList.add('favorite-card');
+
+            if (currentBackgroundColor === 'rgb(240, 240, 240)') {
+                favoriteCard.style.backgroundColor = 'white';
+                favoriteCard.style.color = 'black';
+            } else {
+                favoriteCard.style.backgroundColor = '#18181b';
+                favoriteCard.style.color = 'white';
+            }
+
+            favoriteCard.innerHTML = `
+            <p><em>"${quotes[currentIndex].quote}"</em></p>
+            <p>${quotes[currentIndex].author}</p>`;
+            
+            favoriteContainer.appendChild(favoriteCard);
+        } else {
+            const favoriteCards = document.querySelectorAll('.favorite-card');
+            
+            favoriteCards.forEach(card => {
+                if (card.textContent.includes(quotes[currentIndex].quote)) {
+                    card.remove();
+                }
+            });
+        }
+
+    } catch (error) {
         quoteElement.style.color = 'red';
         quoteElement.textContent = 'Quote has not been generated yet.';
         
+        console.log(error.stack);
+
         setTimeout(() => {
             if (currentBackgroundColor === 'rgb(240, 240, 240)') {
                 quoteElement.style.color = 'black';
@@ -86,6 +119,7 @@ function switchMode() {
     const currentBackgroundColor = getComputedStyle(body).backgroundColor;
 
     if (currentBackgroundColor === 'rgb(240, 240, 240)') {
+        const favoriteCards = document.querySelectorAll('.favorite-card');
 
         body.style.backgroundColor = '#27272a';
         container.style.backgroundColor = '#18181b';
@@ -93,13 +127,19 @@ function switchMode() {
         heading1.style.color = '#f0f0f0';
         dayNightModeButton.style.backgroundColor = '#27272a';
         dayNightModeButton.style.color = '#f0f0f0';
-        dayNightModeButton.textContent = 'Switch to light theme'
+        dayNightModeButton.textContent = 'Switch to light theme';
         authorElement.style.color = '#f0f0f0';
         favoriteButton.style.backgroundColor = '#27272a';
         favoriteButton.style.color = '#f0f0f0';
+        
+        favoriteCards.forEach(card => {
+            card.style.backgroundColor = '#18181b';
+            card.style.color = 'white';
+        })
     
     } else {
-        
+        const favoriteCards = document.querySelectorAll('.favorite-card');
+
         body.style.backgroundColor = '#f0f0f0';
         container.style.backgroundColor = 'rgb(255, 255, 255)'
         quoteElement.style.color = 'black';
@@ -110,6 +150,11 @@ function switchMode() {
         authorElement.style.color = 'black';
         favoriteButton.style.backgroundColor = 'aliceblue';
         favoriteButton.style.color = 'black';
+
+        favoriteCards.forEach(card => {
+            card.style.backgroundColor = 'white';
+            card.style.color = 'black';
+        })
     }
 }
 
